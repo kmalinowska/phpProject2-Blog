3b6b3f05121aa252b730fa0af501aa7c8e475bc4
Klaudia Malinowska <kmalinowska@protonmail.com>
2026-02-10 19:48:25 +0100

    feat: add create method to Model for database inserts

--- core/Model.php
+++ core/Model.php
@@ -15,15 +15,25 @@ abstract class Model {
         $result = $db->query("SELECT * FROM " . static::$table . " WHERE id = ?", [$id])->fetch(PDO::FETCH_ASSOC);
         return $result ? static::createFromArray($result) : null;
     }
-    public static function create(array $data): static {}
+    public static function create(array $data): static {
+        $db = App::get('database');
+        // 1. Get the names of columns inside $data
+        $columns = implode(', ', array_keys($data));
+        // -> id, title, created_at, content
+        $placeholders = implode(', ', array_fill(0, count($data), '?'));
+        // -> ?, ?, ?, ?
+        $sql = "INSERT INTO " . static::$table . " ($columns) VALUES ($placeholders)";
+        $db->query($sql, array_values($data));
+        return static::find($db->lastInsertId());
+       }
 
     protected static function createFromArray(array $data): static {
         $model = new static();
         foreach($data as $key => $value) {
             if (property_exists($model, $key)) {
                 $model->$key = $value;
-            }
-            return $model;
+            } 
         }
+        return $model;
     }
 }
