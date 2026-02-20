592b2047bb6e7ba73859421443be44f8f803e6ac
Klaudia Malinowska <kmalinowska@protonmail.com>
2026-02-20 10:27:26 +0100

    refactor: extract posts list into _posts partial and add global partial helper

--- public/index.php
+++ public/index.php
@@ -8,6 +8,7 @@ use Core\Router;
 $router = new Router();
 
 require_once __DIR__ . '/../routes.php';
+require_once __DIR__ . '/../helpers.php';
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
